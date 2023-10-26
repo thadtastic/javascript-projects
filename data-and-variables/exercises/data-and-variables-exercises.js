4f3399aca627933c90ecdcbe7dea5183cb2fd272
thadtastic <leothaddeus92@gmail.com>
2023-10-26 15:03:00 -0500

    declared and assigned variables, printed variable types

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,7 +1,15 @@
 // Declare and assign the variables below
-
+let nameOfSpaceShuttle = "Determination";
+let shuttleSpeed = 17500;
+let distanceToMars = 225000000;
+let distanceToMoon = 384400;
+const milesPerKilometer = 0.621;
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log( typeof nameOfSpaceShuttle);
+console.log( typeof shuttleSpeed);
+console.log( typeof distanceToMars);
+console.log( typeof distanceToMoon);
+console.log( typeof milesPerKilometer);
 // Calculate a space mission below
 
 // Print the results of the space mission calculations below
