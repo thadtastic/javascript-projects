f9bffe8b6db35d9ffa1109f6507bc4f8ebca56bc
thadtastic <leothaddeus92@gmail.com>
2023-10-31 11:19:55 -0500

    numbers 1-2 completed of part three of stringing character together exercise

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,9 +3,14 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
-
+let firstLetter = language.slice(0,1);
+let secondLetter = language.slice(4,5);
+console.log(firstLetter + secondLetter);
+// after checking for solution
+console.log(language.slice(0,1)+ language.slice(4,5));
+let initials = (language.slice(0,1)+ language.slice(4,5));
 //2. Without using slice(), use method chaining to accomplish the same thing.
-
+console.log(language.replace("ava", "")+language.replace("cript", ""));
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
