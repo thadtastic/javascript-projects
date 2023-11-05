23e94588f5d2eb32c8f13cb4c76762ec38413c4e
thadtastic <leothaddeus92@gmail.com>
2023-11-04 21:17:00 -0500

    added elements to arrays

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,5 +1,12 @@
 //Create an array that can hold 4 items name practiceFile.
-
+let practiceFile = ["item1","item2","item3","item4"]
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
-
+practiceFile[4] = "42";
+practiceFile[5] = "hello";
+let false = false 
+let
 //Use a SetValue to add the items "false", and "-4.6" to the array.  Print the array to confirm the changes.
+//*Research* setValue sets the value of an element using a namespace
+practiceFile.push(false,-4.6,"87");
+console.log(practiceFile);
+
