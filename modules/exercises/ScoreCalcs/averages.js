2431b5ffd2c97f182bb53c85dc6947fe219d51b7
thadtastic <leothaddeus92@gmail.com>
2023-11-13 15:47:17 -0600

    excercise module exporting

--- modules/exercises/ScoreCalcs/averages.js
+++ modules/exercises/ScoreCalcs/averages.js
@@ -17,3 +17,7 @@ function averageForTest(testIndex,scores){
 }
 
 //TODO: Export all functions within an object.
+module.export ={
+  averageForStudent: averageForStudent,
+  averageForTest: averageForTest
+}
--- modules/exercises/display.js
+++ modules/exercises/display.js
@@ -1,5 +1,6 @@
 //TODO: Export ONLY the printAll function.
 
+
 function printAll(names, tests, scores){
   let header = 'Name';
   let row = '';
@@ -34,3 +35,4 @@ function printTestScores(index,test,students,scores){
   }
   return;
 }
+module.export = printAll;
