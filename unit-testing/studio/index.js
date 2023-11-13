345d76eaeffbbff2758539d6cf4bac2d8183aeaf
thadtastic <leothaddeus92@gmail.com>
2023-11-13 15:42:37 -0600

    unit test studio, creation of 9 tests

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,14 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(num){
+    if(num % 2 === 0)
+    return "launch!"
+
+  }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,37 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+  test("organization should return value of nonprofit ", function(){
+    expect(launchcode.organization).toEqual("nonprofit")
+  });
+  test("executiveDirector should return value of jeff ", function(){
+      expect(launchcode.executiveDirector).toEqual("jeff")
   
+  });
+  test("percentageCoolEmployees should return value of 100 ", function(){
+    expect(launchcode.percentageCoolEmployees).toEqual(100)
+
+});
+test("executiveDirector should return value of jeff ", function(){
+  expect(launchcode.executiveDirector).toEqual("jeff")
+
+});
+test("programsOffered should contain value of Web Development ", function(){
+  expect(launchcode.programsOffered).toContain("Web Development")
+
+});
+test("programsOffered should contain value of Data Analysis ", function(){
+  expect(launchcode.programsOffered).toContain("Data Analysis")
+
+});
+test("programsOffered should contain value of Liftoff ", function(){
+  expect(launchcode.programsOffered).toContain("Liftoff")
+
+});
+test("Programs offered should have three elements", function(){
+  expect(launchcode.programsOffered.length).toBe(3)
+});
+test("Number should be divisble by 2", function(){
+  expect(launchcode.launchOutput(16)).toEqual("launch!")
+})
 });
