93122a7c8412682eb3135f92613885f1e6dee204
thadtastic <leothaddeus92@gmail.com>
2023-12-25 15:35:07 -0600

    exercises and studio changes

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,23 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function(){
+        paragraph.innerHTML = "Houston, we have liftoff!"
+    })
+    missionAbort.addEventListener("mouseover", event =>{
+        event.target.style.backgroundColor = "red";
+    })
+    missionAbort.addEventListener("mouseout", event =>{
+        //event.target.style.backgroundColor = ""
+        let element = event.target; 
+        element.style.backgroundColor = ""
+    })
+    missionAbort.addEventListener("click", function(){
+        let confirmation = window.confirm("Are you sure you want to abort the mission?");
+            if (confirmation === true){
+                paragraph.innerHTML = "Mission aborted! Space shuttle returning home"
+            }
+    })
 }
 
 window.addEventListener("load", init);
--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,29 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (res) {
+        res.json().then(function (data) {
+            let astros = document.getElementById("container")
+            //astros.innerHTML = data()
+
+            for (let i = 0; i < data.length; i++) {
+                let greenTrue = document.getElementById("green-true")
+                if(data[i].active){
+                    greenTrue.style.color = "green";
+                }
+                astros.innerHTML +=`
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${data[i].firstName}</h3>
+                            <ul>
+                                <li>Hours in space: ${data[i].hoursInSpace}</li>
+                                <li id="green-true">Active: ${data[i].active}</li>
+                                <li>Skills: ${data[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${data[i].picture}">
+                    </div>`
+            }
+        
+        })
+    })
+})
